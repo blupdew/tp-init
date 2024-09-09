b09f80406c85c7a44631be4003b164cc6d7786b9
Rodolphe <rodolphe@alsacreations.fr>
2024-09-09 22:25:42 +0200

    feat: everything

---
+++ vite-project/vite.config.ts
@@ -0,0 +1,9 @@
+// vite.config.js
+// https://vitejs.dev/guide/build.html
+
+import UnoCSS from "unocss/vite"; // TODO: Était déjà présent
+
+export default {
+  // config options
+  plugins: [UnoCSS()],
+};
